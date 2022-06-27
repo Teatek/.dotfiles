8ab83e339babca53dd252a16ea7b99b5686c0ead
Teatek <38403802+Teatek@users.noreply.github.com>
2022-06-27 17:58:13 +0200

    Packer auto install and sync

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,4 +1,11 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
   use 'romainl/vim-qf'
   use 'altercation/vim-colors-solarized'
   use 'navarasu/onedark.nvim' 
@@ -49,4 +56,10 @@ return require('packer').startup(function(use)
 
   -- rest api
   use 'NTBBloodbath/rest.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
