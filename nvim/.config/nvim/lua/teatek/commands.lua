11251175dbba2de83369aed49e9a485a7934c929
Teatek <teatek0512@gmail.com>
2022-09-07 09:49:36 +0200

    error format when using make for java and c#

--- nvim/.config/nvim/lua/teatek/commands.lua
+++ nvim/.config/nvim/lua/teatek/commands.lua
@@ -1,4 +1,13 @@
 -- quickfix list
--- vim.api.nvim_create_autocmd("FileType cs", {
---   command = "setlocal errorformat= %#%f(%l\\,%c): %m"
--- })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cs",
+  command = "setlocal errorformat=%#%f(%l\\,%c): %m"
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  command = "setlocal makeprg=javac\\ %"
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  command = "setlocal errorformat=%A%f:%l:\\ %m,%+Z%p^,%+C%.%#,%-G%.%#"
+})
