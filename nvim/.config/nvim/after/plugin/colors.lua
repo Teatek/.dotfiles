d19031842df2a90ec2079f1467601b841df8dd2a
Teatek <teatek0512@gmail.com>
2023-04-03 01:21:33 +0200

    Set theme automatically

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -5,4 +5,18 @@ require('lualine').setup {
     -- ... your lualine config
   }
 }
+
+-- set colorscheme automatically based on terminal theme
+vim.api.nvim_create_autocmd("OptionSet", {
+  pattern = "background",
+  callback = function()
+    if vim.g.background == 'dark' then
+      vim.cmd.colorscheme('nightfox')
+    else
+      vim.cmd.colorscheme('dayfox')
+    end
+  end
+})
+
+-- set colorscheme by default
 vim.cmd[[colorscheme nightfox]]
